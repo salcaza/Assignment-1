78ae0a2073b6db3945370326c758330eff74ab96
Sergio Alcazar <sergioalcazar1007@gmail.com>
2026-04-22 18:34:31 -0700

    Added create_file and delete_file functions

--- a1.py
+++ a1.py
@@ -4,6 +4,22 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# NAME Sergio Alcazar
+# EMAIL smalcaza@uci.edu
+# STUDENT ID 73371481
+
+import shlex
+from pathlib import Path 
+
+def create_file(path_str, name):
+    directory = Path(path_str)
+    if not directory.exists() or not directory.is_dir():
+        print("ERROR")
+        return
+    new_file = directory / (name + ".dsu")
+    if new_file.exists():
+        print("ERROR")
+        return
+    new_file.touch()
+    print(new_file)
+
